64f327b972f6bb02bd1c5001e91ba48c1696c938
Kevin Nash <kieve10@hotmail.com>
2012-09-18 13:34:11 -0400

    Simplified code and cleanup. made several changes to make the code easier to read and follow.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+var global = {
+	space: 32,
+	nbsp: 160
+}
+
 var dict = {
 	wordsWith: function (letters) {
 		console.log(letters);
@@ -20,27 +25,40 @@ var dict = {
 
 var lessons = {
 	data: ["uh", "et", "on", "as", "id", "pg", ".c", ",r", ";l", "yf", "km", "jw", "qv", "'z", "xb"],
+
+	/**
+	 * Examples: load(0.5); load(14); load(1);
+	 */
 	load: function(num) {
-		letters = "";
-		var index = Math.floor(num / 2);
-		if (num % 2 == 0) {
-			letters = lessons.data[index];
+		// All letters up to this lesson.
+		var cumulative = [];
+		// Letters specific to this lesson.
+		var current = [];
+
+		var index = Math.floor(num);
+		current = lessons.data[index].split("");
+
+		// Check for decimals.
+		if (num % 1 == 0) {
+			cumulative = current;
 		} else {
-			for (var i = 0; i < num / 2; i++) {
-				letters += lessons.data[i];
+			for (var i = 0; i < index + 1; i++) {
+				cumulative = cumulative.concat(lessons.data[i].split(""));
 			}
-		} 
+		}
+
 		var punctuation = [" "];
 		$.each('.,;', function (i, ch) {
-			if (letters.indexOf(ch) != -1) punctuation = punctuation.concat([ch + ' ', ' ']);
+			if (cumulative.contains(ch)) {
+				punctuation = punctuation.concat([ch + ' ', ' ']);
+			}
 		});
 		
-		var lettersArray = letters.split("");
-		var title = num / 2 + ': "' + lettersArray[0] + '", "' + lettersArray[1] + '"';
+		var title = num + ': "' + current.join('", "') + '"';
 		return {
-			letters: lettersArray,
+			letters: cumulative,
 			punctuation: punctuation,
-			words: dict.wordsWith(lettersArray),
+			words: dict.wordsWith(cumulative),
 			title: title
 		};
 	}
@@ -61,20 +79,19 @@ function loadLesson(num) {
 	el.children().remove();
 	$.each(sentence, function (i, ch) {
 		// We use nbsp here so that the last character on the line is shown.
-		if (ch == " ") ch = String.fromCharCode(160);
+		if (ch == " ") ch = String.fromCharCode(global.nbsp);
 		$("<span>").text(ch).appendTo(el);
 	});
 	el.children(':first-child').addClass('current');
 
 	var prevts = 0;
 	var total = 0;
-	var count = 0;
 	var errors = 0;
 	$('body').off('keypress.dtt');
 	$('body').on('keypress.dtt', function (ev) {
 		if (prevts == 0) prevts = new Date();
 
-		var typed = String.fromCharCode(ev.which == 32 ? 160 : ev.which);
+		var typed = String.fromCharCode(ev.which == global.space ? global.nbsp : ev.which);
 		var cur = $('#current-lesson .current');
 		if (cur.text() != typed) {
 			if (!cur.hasClass('failed')) {
@@ -86,24 +103,19 @@ function loadLesson(num) {
 			return;
 		}
 
-		if (ev.which == 32) {
-			var newts = new Date();
-			var delta = newts.getTime() - prevts.getTime();
-			prevts = newts;
-
-			if (delta > 10000) delta = 0; // Ignore long pauses.
-			total += delta;
-			count++;
-			console.log(total, count);
-		}
+		var newts = new Date();
+		var delta = newts.getTime() - prevts.getTime();
+		prevts = newts;
+		if (delta > 10000) delta = 0; // Ignore long pauses.
+		total += delta;
 
 		if (cur.next().length == 0) {
-			var avg = total / count;
+			var avg = total / (sentence.length / 5);
 			var wpm = 60000 / avg;
 			$('#stats-speed').text(wpm.toFixed(0) + ' wpm');
-			console.log(total, count, avg, errors);
+			console.log(total, avg, errors);
 			if (wpm < 30 || errors / sentence.length > 0.05) return loadLesson(num);
-			else return loadLesson(num + 1);
+			else return loadLesson(num + 0.5);
 		} else {
 			cur.removeClass('current').next().addClass('current');
 		}
