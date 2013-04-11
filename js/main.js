a9cda6c2f3bbd8939dbb7fc9332d6ed733b35529
crazy2be <crazy1be@gmail.com>
2013-04-10 23:42:32 -0400

    Fixed a ReferenceError

--- js/main.js
+++ js/main.js
@@ -32,9 +32,10 @@ var lessons = {
 		var cumulative = [];
 
 		var index = Math.floor(num);
+		var current = lessons.data[index].split("");
 
 		if (num % 1 == 0) {
-			cumulative = lessons.data[index].split("");
+			cumulative = current;
 		} else {
 			for (var i = 0; i < index + 1; i++) {
 				cumulative = cumulative.concat(lessons.data[i].split(""));
